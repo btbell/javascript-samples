a793d21c23a2d292ce06f8c68b2eaa33f905f928
Brian Bell <btbell@gmail.com>
2016-10-01 16:18:19 -0400

    add browser check to script

--- ajax_sync_vs_async/scripts/ajax.js
+++ ajax_sync_vs_async/scripts/ajax.js
@@ -1,8 +1,15 @@
 /* create a simple XHR request, either synchronous or asynchronous
 depending on if URL condition is met */
 
+var request;
+// check for older browsers
+if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+} else {
+  request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
 if (window.location.href === 'http://localhost/JavaScript/js-ajax/js-ajax.htm') {
-  var request = new XMLHttpRequest();
     request.open('GET', 'data.txt', false);
     request.send();
     if (request.status === 200) {
