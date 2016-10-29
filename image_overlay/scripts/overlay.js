3ea1fbe59eb1910125d5023798d2bb3bd2735448
Brian Bell <btbell@gmail.com>
2016-10-29 00:51:18 -0400

    final changes to overlay exercise'

--- image_overlay/scripts/overlay.js
+++ image_overlay/scripts/overlay.js
@@ -23,9 +23,76 @@
         myOverlay.style.top = window.pageYOffset + 'px';
         myOverlay.style.left = window.pageXOffset + 'px';
 
+        // create an element for the image
+        var imageLocation = e.target.src;
+        //console.log(e.target);
+        var lgImg = document.createElement('img');
+        lgImg.id = 'lgImg';
+        lgImg.src = imageLocation.substr(0, imageLocation.length-8) + '.jpg'
+        lgImg.style.display = 'block';
+        lgImg.style.position = 'absolute';
+        
+        // wait for image to load and then measure
+        lgImg.addEventListener('load', function() {
+          // resize when image height is taller
+          if (this.height > window.innerHeight) {
+            this.ratio = window.innerHeight / this.height;
+            this.height = this.height + this.ratio;
+            this.width = this.width + this.ratio;
+          }
+
+          // resize when image height is wider
+          if (this.width > window.innerWidth) {
+            this.ratio = window.innerWidth / this.width;
+            this.height = this.height + this.ratio;
+            this.width = this.width + this.ratio;  
+          }
+
+          centerImg(this); // this is the lgImg
+          myOverlay.appendChild(lgImg);
+          
+        });
+
+        lgImg.addEventListener('click', function() {
+          if (myOverlay) {
+            window.removeEventListener('resize', window, false); // clean up
+            window.removeEventListener('scroll', window, false); // clean up
+            myOverlay.parentNode.removeChild(myOverlay);
+          }
+        }, false)
+
+        // adjust for window scroll
+        window.addEventListener('scroll', function(){
+          if (myOverlay) {
+            myOverlay.style.top = window.pageYOffset + 'px';
+            myOverlay.style.left = window.pageXOffset + 'px';
+          }
+        }, false)
+
+        // adjust for window resize
+        window.addEventListener('resize', function() {
+          if (myOverlay) {
+            myOverlay.style.width = window.innerWidth + 'px';
+            myOverlay.style.height = window.innerHeight + 'px';
+            myOverlay.style.top = window.pageYOffset + 'px';
+            myOverlay.style.left = window.pageXOffset + 'px';
+
+            centerImg(lgImg);
+          }
+        }, false)
 
     } // target is the image tag
     
-  }, false)
+  }, false);
+
+  function centerImg(theImage) {
+    var myDifX = (window.innerWidth - theImage.width)/2;
+    var myDifY = (window.innerHeight - theImage.height)/2;
+
+    theImage.style.top = myDifY + 'px';
+    theImage.style.left = myDifX + 'px';
+
+    return theImage;
+  }
 
 })(); // auto loading function
